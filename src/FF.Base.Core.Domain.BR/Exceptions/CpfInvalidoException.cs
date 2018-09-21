6824a7076644e6426e0edb40a5dbbb323aec86c0
Ivan Borges <ivanborges@live.com>
2018-09-21 15:42:09 -0300

    Ajuste nas descrições dos pacotes e mudança de pacote Domain para Domain.BR.

---
+++ src/FF.Base.Core.Domain.BR/Exceptions/CpfInvalidoException.cs
@@ -0,0 +1,10 @@
+﻿using FF.Base.Core.Exceptions;
+
+namespace FF.Base.Core.Domain.BR.Exceptions
+{
+    public class CpfInvalidoException : CoreException
+    {
+        public override string Key => "CpfInvalido";
+        public override string Message => "O CPF fornecido está em branco, é inválido ou está em um formato inválido.";
+    }
+}
