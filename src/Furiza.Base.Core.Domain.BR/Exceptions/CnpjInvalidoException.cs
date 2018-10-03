7b42d10a40816c364d58cd3552688b435b65050a
Ivan Borges <ivanborges@live.com>
2018-10-03 11:01:52 -0300

    Inclusão CNPJ.

---
+++ src/Furiza.Base.Core.Domain.BR/Exceptions/CnpjInvalidoException.cs
@@ -0,0 +1,10 @@
+﻿using Furiza.Base.Core.Exceptions;
+
+namespace Furiza.Base.Core.Domain.BR.Exceptions
+{
+    public class CnpjInvalidoException : CoreException
+    {
+        public override string Key => "CnpjInvalido";
+        public override string Message => "O CNPJ fornecido está em branco, é inválido ou está em um formato inválido.";
+    }
+}
