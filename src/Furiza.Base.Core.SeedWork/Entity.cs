278ec7f8a66a3faa79a996e33bbc0f1ff62053ff
Ivan Borges <ivanborges@live.com>
2018-10-21 17:06:57 -0300

    Ajuste no GetHashCode da classe Entity.

--- src/Furiza.Base.Core.SeedWork/Entity.cs
+++ src/Furiza.Base.Core.SeedWork/Entity.cs
@@ -29,16 +29,10 @@ namespace Furiza.Base.Core.SeedWork
                 return item.Id == Id;
         }
 
-        int? _requestedHashCode;
         public override int GetHashCode()
         {
             if (!IsTransient())
-            {
-                if (!_requestedHashCode.HasValue)
-                    _requestedHashCode = Id.GetHashCode() ^ 31;
-
-                return _requestedHashCode.Value;
-            }
+                return Id.GetHashCode() ^ 31;
             else
                 return base.GetHashCode();
         }
